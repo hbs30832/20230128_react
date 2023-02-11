3c8a7acd2550097a98da04139bacfbcb58dc6e75
hbs30832 <hbs9312@gmail.com>
2023-02-11 10:45:54 +0900

    초기 설정

---
+++ movie_list/src/component/practice/Todos.jsx
@@ -0,0 +1,16 @@
+// src/component/practice/Todos.jsx
+function Todos() {
+  return (
+    <div>
+      <div>
+        <input type="text" />
+        <button>등록</button>
+      </div>
+      <ul>
+        <li>할일</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Todos;
